96721deaa51f8e47a8659cd6ae4a4eacb7059d24
Erik Thomas <erikb.thomas@gmail.com>
2025-10-23 14:20:29 -0400

    Update main.R

--- R/main.R
+++ R/main.R
@@ -42,7 +42,11 @@ NCREIF<-function(SelectString,WhereString,GroupByString,Username,Password,DataTy
     results$Date<-data.frame(Date=as.Date(paste(substr(results$YYYYQ,1,4),as.numeric(substr(results$YYYYQ,5,5))*3,ifelse(substr(results$YYYYQ,5,5) %in% c("4","1"),31,30),sep="-"),"%Y-%m-%d"))$Date
   }
   if(verbose){return(t1)}else{
-    for(i in which(!colnames(results) %in% c(strsplit(gsub("\\]","",gsub("\\[","",GroupByString)),",")[[1]],"Date","Tranche"))){results[,i]<-as.numeric(results[,i])}
+    for(i in which(!colnames(results) %in% c(strsplit(gsub("\\]","",gsub("\\[","",GroupByString)),",")[[1]],"Date","Tranche"))){
+      if(any(!is.na(as.numeric(results[,i])))){
+        results[,i]<-as.numeric(results[,i])}
+    
+    }
     return(results)}
   }else{
 
